f41ca7a9ff2a977a032446dd58806c52b6d7fbb1
codefalling <code.falling@gmail.com>
2016-01-10 07:32:31 +0000

    disable maximized

--- init.el
+++ init.el
@@ -37,7 +37,8 @@ values."
      markdown
      javascript
      org
-     (elfeed :variables rmh-elfeed-org-files (list "~/Dropbox/org-notes/gtd.org"))
+;;     (elfeed :variables rmh-elfeed-org-files (list 
+;;"~/Dropbox/org-notes/gtd.org"))
 
      ;; (shell :variables
      ;;        shell-default-height 30
@@ -178,7 +179,7 @@ values."
    ;; If non nil the frame is maximized when Emacs starts up.
    ;; Takes effect only if `dotspacemacs-fullscreen-at-startup' is nil.
    ;; (default nil) (Emacs 24.4+ only)
-   dotspacemacs-maximized-at-startup t
+   dotspacemacs-maximized-at-startup nil
    ;; A value from the range (0..100), in increasing opacity, which describes
    ;; the transparency level of a frame when it's active or selected.
    ;; Transparency can be toggled through `toggle-transparency'. (default 90)
@@ -266,5 +267,5 @@ layers configuration. You are free to put any user code."
 "))
 
   (spacemacs/toggle-mode-line-org-clock-on)
-  (add-to-list 'after-make-frame-functions (lambda () (toggle-frame-maximized)))
+  ;; (add-to-list 'after-make-frame-functions (lambda () (toggle-frame-maximized)))
   )
